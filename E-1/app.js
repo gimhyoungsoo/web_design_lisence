2aee2545440b2bc93dd5273ce46346a33853ad1b
김형수 <gimhyoungsoo@gmail.com>
2023-03-12 16:23:43 +0900

    element.style.att => animate(keyframe,KeyframeEffect)로 변경
    // 첫번째 node를 cloneNode해 appendChild하는 방식은 수동조작 carousel에서 적합하다고 판단됨 (마지막 노드에서 첫번째 노드로 넘아간뒤 다시 슬라이드가 시작되는 부분에서 setInterval이 두번 반복되어 첫번째 node가 다른 노드보다 두배 많이 노출됨)

--- E-1/app.js
+++ E-1/app.js
@@ -20,23 +20,38 @@ function ModalInit() {
 }
 
 function SlideInit() {
-    const slide = document.querySelector("#B .slide_container")
-    slide.append(slide.firstElementChild.cloneNode(true))
-    let i = 0;
-    let timerId = setInterval(() => {
-        
-        if ((i < 4)) {
-            slide.style.transition = `transform 0.5s`
-            slide.style.transform = `translateX( calc( ${-i * 100}vw + ${i * 600}px ))`
-            i++;
-           
+    const slide_wrap = document.querySelector("#B .slide_container")
+    slide_wrap.append(slide_wrap.firstElementChild.cloneNode(true))
+    const slide_nodelist = document.querySelectorAll("#B .slide_container div")
+    // const slide_arr = Array.from(slide_nodelist)
+
+
+    const options = {
+        duration: 500,
+        iterations: 1,
+        fill: "both",
+    };
+    let i = 0
+    const intervalId = setInterval(
+        // 첫번째 node를 cloneNode해 appendChild하는 방식은 수동조작 carousel에서 적합하다고 판단됨 (마지막 노드에서 첫번째 노드로 넘아간뒤 다시 슬라이드가 시작되는 부분에서 setInterval이 두번 반복되어 첫번째 node가 다른 노드보다 두배 많이 노출됨)
+        Slide,
+        3000)
+
+    function Slide() {
+        if (i < 3) {
+            i++
+            console.log(i)
+            slide_wrap.animate(
+                { marginLeft: `-${i * 100}%` }
+                , options)
         }
         else {
-            slide.style.transition = `transform 0s`
-            slide.style.transform = `translateX(0)`
+            
+            slide_wrap.animate(
+                { marginLeft: `0` }, { duration: 0, fill: "forwards" }
+            )
             i = 0
         }
-        
-        
-    },1500)
+
+    }
 }
